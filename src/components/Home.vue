94f7d83b16e13410d728bcf344bdd3b37a752c1e
ALICE LIANG <a0975012712@gmail.com>
2023-03-16 13:22:39 +0800

    no message

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,49 @@
 <template>
-  <div>Hello this is home page</div>
+  
+  
+  <div>
+    <input type="text" v-model="search" placeholder="search blogs" />
+    <b-conainter>
+    <b-row>
+    <b-card
+      v-for="blog in filteredBlogs"
+      :key="blog.id"
+      :title="blog.title"
+      img-src="https://picsum.photos/600/300/?image=25"
+      img-alt="Image"
+      img-top
+      tag="article"
+      style="max-width: 20rem;"
+      class="mb-2"
+    >
+      <b-card-text>
+        {{ blog.body }}
+      </b-card-text>
+
+      <b-button href="#" variant="primary">See More</b-button>
+    </b-card>
+  </b-row>
+</b-conainter>
+  </div>
 </template>
 
 <script>
+import searchMixin from '../mixins/searchMixin';
 export default {
   data() {
     return {
+      blogs: [],
+      search: '',
     };
   },
+  created() {
+    this.$http
+      .get("http://jsonplaceholder.typicode.com/posts")
+      .then(function(data) {
+        this.blogs = data.body.slice(0, 10);
+      });
+  },
+  mixins: [searchMixin]
 };
 </script>
 
